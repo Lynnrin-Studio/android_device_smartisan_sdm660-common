f6739ed185d50502d2c933abf4cdbde0cd5a8c06
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-28 16:03:32 -0700

    sdm660-common: Use libprotobuf-cpp-full-v29.so for libsettings.so
    * The symbol `_ZN6google8protobuf8internal13empty_string_E` was
      removed in the API30 version of this lib. Copy the old one
      and patchelf in the dependency.
    
    Change-Id: I3553c9cbb6ccff202aab706b7b7fa2f39d1480a9

--- common.mk
+++ common.mk
@@ -409,6 +409,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
